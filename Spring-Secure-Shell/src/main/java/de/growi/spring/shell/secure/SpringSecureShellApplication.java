04241b615fcf21e2fcdd74954ed8678febc56d11
growi <growi@ankertaugasse.de>
2017-12-22 02:11:25 +0100

    Initial Commit

---
+++ Spring-Secure-Shell/src/main/java/de/growi/spring/shell/secure/SpringSecureShellApplication.java
@@ -0,0 +1,12 @@
+package de.growi.spring.shell.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecureShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecureShellApplication.class, args);
+	}
+}
